ff3a83f252077d520266ee704079c10a93946bfd
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-06-26 15:43:09 -0400

    set selection -2 HERE +2 first ; + multicursor

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,39 @@
-import * as vscode from 'vscode'
+import {commands,window,Selection} from 'vscode'
+import type {ExtensionContext} from 'vscode'
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext): void {
 
-  console.log('Congratulations, your extension "git-commits-editor" is now active!')
+  context.subscriptions.push(commands.registerCommand('git-commits-editor.helloWorld',() => {
 
+    const activeEditor = window.activeTextEditor
+    if (activeEditor) {
 
-  context.subscriptions.push(vscode.commands.registerCommand('git-commits-editor.helloWorld', () => {
+      const selectionArr = activeEditor.selections
+      const newSelectionArr: Selection[] = []
+      for (let i = 0,len = selectionArr.length; i < len; i++) {
+        const selection = selectionArr[i]
+        const active = selection.active
+        const start = selection.start
+        const end = selection.end
+        console.log(selectionArr[i].active)
 
-    vscode.window.showInformationMessage('Hello World from git-commits-editor!')
+        let activeEqualStart = false
+        if (active.character === start.character && active.line === start.line) {
+          activeEqualStart = true //and anchor===end
+        }
+        let selAnchor,selActive
+        if (activeEqualStart) {
+          selActive = start
+          selAnchor = end
+        } else {
+          selActive = end
+          selAnchor = start
+        }
+        /// new Selection(anchorLine: number, anchorCharacter: number, activeLine: number, activeCharacter: number): Selection
+        newSelectionArr.push(new Selection(selAnchor.line,selAnchor.character - 2,selActive.line,selActive.character + 2))
+      }
+      activeEditor.selections = newSelectionArr
+    }
+
   }))
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
