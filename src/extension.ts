ab225cd2bb29f1f18f9b6f00f4218b840e84bda2
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-06-26 20:04:18 -0400

    omg, configuration has to be in contributes

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 import {commands,window,workspace,Selection} from 'vscode'
-import type {ExtensionContext,Position} from 'vscode'
+import type {ExtensionContext} from 'vscode'
 
 export function activate(context: ExtensionContext): void {
   const min = Math.min
   const d = console.debug.bind(console)
 
-  // const bracketPairs = workspace.getConfiguration('bracket-select').get('bracketPairs');
-  const sameLineSameBracketArr: string[] | undefined = workspace.getConfiguration('bracket-select').get('sameLineSameBracket')
+  // const bracketPairs = workspace.getConfiguration('vscode-bracket-select').get('bracketPairs');
+  d(workspace.getConfiguration('vscode-bracket-select'))
+  const sameLineSameBracketArr: string[] | undefined = workspace.getConfiguration('vscode-bracket-select').get('sameLineSameBracket')
   const sameLineSameBracketObj: stringIndexString = {}
   if (sameLineSameBracketArr) {
     for (let i = 0,len = sameLineSameBracketArr.length; i < len; i++) {
@@ -14,19 +15,28 @@ export function activate(context: ExtensionContext): void {
     }
   }
 
-  context.subscriptions.push(commands.registerCommand('git-commits-editor.helloWorld',() => {
+  context.subscriptions.push(commands.registerCommand('vscode-bracket-select.helloWorld',() => {
 
     const activeEditor = window.activeTextEditor
     if (activeEditor) {
 
       const lines = activeEditor.document.getText().split('\n')
 
-      function selectionFromActive(active: Position) {
-        const c = active.character,i = active.line,line = lines[i]
-        //check for closest in line, start at left
-        //for both sides
-        //then the rest of the longer one
+      const selectionArr = activeEditor.selections
+      const newSelectionArr: Selection[] = []
+      for (let n = 0,len = selectionArr.length; n < len; n++) {
+        const selection = selectionArr[n]
+        const active = selection.active
+        const start = selection.start
 
+        // return
+
+        let activeEqualStart = false
+        if (active.character === start.character && active.line === start.line) {
+          activeEqualStart = true //and anchor===end
+        }
+
+        const c = active.character,i = active.line,line = lines[i]
         const numberOfChars = lines.length
 
         const leftLen = c - 1
@@ -34,51 +44,67 @@ export function activate(context: ExtensionContext): void {
 
         const lastLeft = c - min(leftLen,rightLen) - 2
 
-        d(lastLeft)
         let rightC = leftLen,leftC
-        for (leftC = c - 1; leftC > lastLeft; leftC--) {
-          console.log(line[leftC])
-          console.log(line[++rightC])
-        }
-        console.log('====================')
-        if (leftLen > rightLen) {
-          while (leftC > -1) {
-            console.log(line[leftC--])
+        //check for closest in line, start at left
+        //for both sides
+        //then the rest of the longer one
+        let lookingFor
+        let c1,c2
+
+        let foundSameLine = true
+        sameLineLabel:
+        while (true) {
+          for (leftC = c - 1; leftC > lastLeft; leftC--) {
+
+            if (sameLineSameBracketObj[line[leftC]]) {
+              lookingFor = sameLineSameBracketObj[line[leftC]]
+              c1 = leftC
+              break sameLineLabel
+            } else if (sameLineSameBracketObj[line[++rightC]]) {
+              lookingFor = sameLineSameBracketObj[line[rightC]]
+              c2 = rightC
+              break sameLineLabel
+            }
           }
-        } else if (rightLen > leftLen) {
-          while (rightC < numberOfChars) {
-            console.log(line[++rightC])
+          // console.log('====================')
+          // if (leftLen > rightLen) {
+          // while (leftC > -1) {
+          // console.log(line[leftC--])
+          // }
+          // } else if (rightLen > leftLen) {
+          // while (rightC < numberOfChars) {
+          // console.log(line[++rightC])
+          // }
+          // }
+          foundSameLine = false
+          break sameLineLabel
+        }
+        d(lookingFor)
+        d(foundSameLine)
+        if (foundSameLine) {
+          if (c1) {
+            while (rightC < numberOfChars) {
+              if (line[++rightC] === lookingFor) {
+                c2 = rightC
+                break
+              }
+            }
+          } else if (c2) {
+            while (leftC > -1) {
+              if (line[leftC--] === lookingFor) {
+                c1 = leftC
+                break
+              }
+            }
+          }
+          if (activeEqualStart) {
+            newSelectionArr.push(new Selection(i,c2 as number,i,c1 as number))
+          } else {
+            newSelectionArr.push(new Selection(i,c1 as number,i,c2 as number))
           }
         }
       }
-
-      const selectionArr = activeEditor.selections
-      const newSelectionArr: Selection[] = []
-      for (let i = 0,len = selectionArr.length; i < len; i++) {
-        const selection = selectionArr[i]
-        const active = selection.active
-        const start = selection.start
-        const end = selection.end
-
-        selectionFromActive(active)
-        return
-
-        let activeEqualStart = false
-        if (active.character === start.character && active.line === start.line) {
-          activeEqualStart = true //and anchor===end
-        }
-        let selAnchor,selActive
-        if (activeEqualStart) {
-          selActive = start
-          selAnchor = end
-        } else {
-          selActive = end
-          selAnchor = start
-        }
-        /// new Selection(anchorLine: number, anchorCharacter: number, activeLine: number, activeCharacter: number): Selection
-        newSelectionArr.push(new Selection(selAnchor.line,selAnchor.character - 2,selActive.line,selActive.character + 2))
-      }
-      // activeEditor.selections = newSelectionArr
+      activeEditor.selections = newSelectionArr
     }
 
   }))
