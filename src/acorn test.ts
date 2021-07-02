62be1aaf67fc767fd9408257f6ff828486cbd6ca
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-07-02 17:21:45 -0400

    positionToIndex(line,char), indexToPosition(index)

---
+++ src/acorn test.ts
@@ -0,0 +1,22 @@
+import * as acorn from 'acorn'
+
+import * as util from 'util'
+
+import * as fs from 'fs'
+// console.log(__dirname + __filename)
+console.log(1 + 1)
+
+// const parsed = acorn.parse(fs.readFileSync(__filename).toString(),{ecmaVersion:2020,sourceType:'module'})
+// const parsed = acorn.parse('"hello"',{ecmaVersion:2020})
+// const parsed = acorn.parse('1+1',{ecmaVersion:2020})
+// const parsed = acorn.parse(`function foo() {
+// }`,{ecmaVersion:2020})
+// const parsed = acorn.parse('{}',{ecmaVersion:2020})
+// const parsed = acorn.parse('v={}',{ecmaVersion:2020})
+// const parsed = acorn.parse('v=``',{ecmaVersion:2020})
+// BlockStatement
+// TemplateLiteral
+// TemplateElement
+
+// ObjectExpression
+console.log(util.inspect(parsed,{showHidden:false,depth:null}))
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
-import {commands,window,workspace,Selection} from 'vscode'
+import {commands,window,workspace,Selection,Position} from 'vscode'
 import type {ExtensionContext} from 'vscode'
+
+import * as acorn from 'acorn'
+import * as walk from 'acorn-walk'
+import {parse} from '@typescript-eslint/typescript-estree'
 
 export function activate(context: ExtensionContext): void {
   const min = Math.min
@@ -32,12 +36,93 @@ export function activate(context: ExtensionContext): void {
     const activeEditor = window.activeTextEditor
     if (activeEditor) {
 
-      const lines = activeEditor.document.getText().split('\n')
+      const documentText = activeEditor.document.getText()
+
+      const totalChars = documentText.length
+
+      const lines = documentText.split('\n')
+
       const howManyLinesMinusOne = lines.length - 1
       const lastLineNumberOfChars = lines[howManyLinesMinusOne].length
 
       const selectionArr = activeEditor.selections
       const newSelectionArr: Selection[] = []
+
+      const linesLenPlusNewline: number[] = []
+      for (let i = 0,len = lines.length; i < len; i++) {
+        linesLenPlusNewline.push(lines[i].length + 1)
+      }
+      linesLenPlusNewline[howManyLinesMinusOne]--
+
+      function indexToPosition(index: number) {
+        if (index > totalChars) {
+          return [howManyLinesMinusOne,lastLineNumberOfChars]
+        }
+        index++
+        let idxAt = 0
+        for (let i = 0,len = linesLenPlusNewline.length; i < len; i++) {
+
+          idxAt += linesLenPlusNewline[i]
+
+          if (idxAt < index) {
+            if (i !== howManyLinesMinusOne) {
+              continue
+            }
+          }
+          idxAt -= linesLenPlusNewline[i]
+          return [i,index - idxAt - 1]
+        }
+        if (index > totalChars) {
+          throw 'index>totalChars'
+        }
+        return [howManyLinesMinusOne,lastLineNumberOfChars]
+      }
+      function createPositionToIndex() {
+        return (line: number,char: number) => {
+          let idxAt = 0
+          for (let i = 0; i < line; i++) {
+            idxAt += linesLenPlusNewline[i]
+          }
+          idxAt += char
+          return idxAt
+        }
+      }
+
+
+      // const selection = selectionArr[n]
+      const selection = activeEditor.selection
+      const active = selection.active
+      const start = selection.start
+      const end = selection.end
+      const positionToIndex = createPositionToIndex()
+      const gotIndex = positionToIndex(active.line,active.character)
+      d(gotIndex)
+      const [startLine,startChar] = indexToPosition(gotIndex)
+      d([startLine,startChar])
+      const [endLine,endChar] = indexToPosition(gotIndex + 1)
+      // d([startLine,startChar])
+      d([endLine,endChar])
+      // activeEditor.selection = new Selection(startLine,startChar,startLine,startChar + 1)
+      // activeEditor.selection = new Selection(startLine,startChar - 1,startLine,startChar)
+      activeEditor.selection = new Selection(startLine,startChar,endLine,endChar)
+      return
+
+      const parsed = parse(documentText,{ecmaVersion:2020,loc:true})
+
+      d(parsed)
+
+      walk.simple(parsed,{
+      // walk.simple(acorn.parse(documentText,{ecmaVersion:2020}),{
+        ObjectExpression(node) {
+          d(`Found a literal: ${node.start}, ${node.end}`)
+          const [startLine,startChar] = indexToPosition(node.start - 1)
+          const [endLine,endChar] = indexToPosition(node.end - 1)
+          // activeEditor.selections = [new Selection(startLine,startChar,startLine,startChar + 1),
+          // new Selection(endLine,endChar,endLine,endChar + 1)]
+          activeEditor.selection = new Selection(startLine,startChar + 1,endLine,endChar)
+        },
+      })
+      return
       labelEachCursor:
       for (let n = 0,len = selectionArr.length; n < len; n++) {
 
@@ -66,46 +151,7 @@ export function activate(context: ExtensionContext): void {
           }
         }
 
-
-        const count = 0
-        while (true) {
-          // count++
-          await commands.executeCommand('editor.action.smartSelect.expand')
-
-          // const selection = selectionArr[n]
-          const selection = activeEditor.selection
-          const active = selection.active
-          const start = selection.start
-          const end = selection.end
-
-          let activeEqualStart = false
-          if (active.character === start.character && active.line === start.line) {
-            activeEqualStart = true //and anchor===end
-          }
-
-          if (start.line === 0 && start.character === 0 && end.line === howManyLinesMinusOne && end.character === lastLineNumberOfChars) {
-            d('HELLO THERE')
-            break
-          }
-          //check if already selected, expand selection to the brackets
-          const leftChar = lines[start.line][start.character - 1] //-1 because there will always be a char if selection {| -> go here |{
-          const rightChar = lines[end.line][end.character]
-          if (anyLeftObj[leftChar]) {
-            if (anyLeftObj[leftChar] === rightChar) {
-              if (activeEqualStart) {
-                newSelectionArr.push(new Selection(end.line,end.character,start.line,start.character))
-              } else {
-                newSelectionArr.push(new Selection(start.line,start.character,end.line,end.character))
-              }
-              continue labelEachCursor
-            }
-          }
-
-        }
-
-
-
-
+        acorn - walk
 
       }
       activeEditor.selections = newSelectionArr
